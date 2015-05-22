ed676eb0d3bfd1f24b2572ee3520a9d17192f80d
sjoulbak <remco_meinen@hotmail.com>
2015-05-22 09:18:53 +0200

    Beautified code and removed functions

--- php/includes/authenticate.php
+++
@@ -1,9 +0,0 @@
-<?php
-function authenticate() {
-  $id = isset($_SESSION['id']) ? $_SESSION['id'] : header('location: /php/login.php');
-  $user = mysqli_query($con, "SELECT id FROM administrator WHERE id =".$_SESSION['id']);
-  if(!$user){
-    header('location: /php/login.php');
-  }
-}
-?>
